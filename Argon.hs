31d235df89e3b9c28cf75768ecafcab26c6c8e45
Quytelda Kahja <quytelda@tamalin.org>
2025-12-05 16:14:21 -0800

    Add Resolve type class and instance for ParseTree

--- Argon.hs
+++ Argon.hs
@@ -23,3 +23,14 @@ instance Alternative ParseTree where
   empty = EmptyNode
   (<|>) = SumNode
   many = ManyNode
+
+class Resolve f where
+  resolve :: f r -> Either String r
+
+instance Resolve ParseTree where
+  resolve EmptyNode         = Left "empty"
+  resolve (ValueNode value) = Right value
+  resolve (MapNode f p)     = fmap f $ resolve p
+  resolve (ProdNode f l r)  = f <$> resolve l <*> resolve r
+  resolve (SumNode l r)     = resolve l <> resolve r
+  resolve (ManyNode p)      = Right []
