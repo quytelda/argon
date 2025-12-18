348c6ce9e04f17bb42d5b36b4457afa113aa8784
Quytelda Kahja <quytelda@tamalin.org>
2025-12-18 03:20:10 -0800

    Move Parser class (and associated classes) to Parser module

--- src/Lib.hs
+++ src/Lib.hs
@@ -19,31 +19,8 @@ import qualified Data.List.NonEmpty        as NonEmpty
 import           Data.Text                 (Text)
 import qualified Data.Text                 as T
 
+import           Parser
 import           Stream
-
--- | Valency represents the maxiumum number of arguments a parser
--- might consume.
---
--- If the valency of a parser is 'Just n', then it consumes up to 'n'
--- arguments. If the valency is 'Nothing', it can consume an arbitrary
--- number of arguments.
-class HasValency p where
-  valency :: p r -> Maybe Integer
-
--- | Things that can be resolved to a value, but might fail to
--- resolve.
-class Resolve f where
-  resolve :: f r -> Except String r
-
--- | A type class for meant to parameterize 'ParseTree's. A parser can
--- consume input token and produce a result or throw an error.
-class Resolve p => Parser (p :: Type -> Type) where
-  data Token p
-  parseTokens :: [Text] -> [Token p]
-  renderTokens :: [Token p] -> [Text]
-
-  accepts :: p r -> Token p -> Bool
-  feedParser :: p r -> MaybeT (Stream (Token p)) r
 
 --------------------------------------------------------------------------------
 -- ParseTree
