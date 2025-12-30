5f451eb2269a47d7c6fec28fb7a4f9aadb9fa95e
Quytelda Kahja <quytelda@tamalin.org>
2025-12-29 19:40:13 -0500

    Add documentation and clean up Stream module.
    Adds pop_ function and removes Context type alias.

--- src/Stream.hs
+++ src/Stream.hs
@@ -4,10 +4,17 @@
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE TypeFamilies          #-}
 
+{-|
+Module      : Stream
+Copyright   : (c) Quytelda Kahja, 2025
+License     : BSD-3-Clause
+
+Provides a basic stream-parsing monad for parsing a token sequences
+with error handling and context management.
+-}
 module Stream
   ( -- * Types
-    Context
-  , ParseResult(..)
+    ParseResult(..)
   , StreamParser(..)
 
     -- * Context
@@ -23,6 +30,7 @@ module Stream
   , pop
   , peek
   , push
+  , pop_
   ) where
 
 import           Control.Applicative
@@ -30,12 +38,11 @@ import           Control.Monad.Except
 import qualified Data.List              as List
 import           Data.Text.Lazy.Builder (Builder)
 
-type Context = Builder
-
+-- | The result of a StreamParser computation.
 data ParseResult tok a
-  = ParseResult [Context] [tok] a
-  | ParseEmpty [Context] [tok]
-  | ParseError [Context] Builder
+  = ParseResult [Builder] [tok] a -- ^ A successful result
+  | ParseEmpty [Builder] [tok] -- ^ The parser doesn't apply
+  | ParseError [Builder] Builder -- ^ The parser failed
   deriving (Functor)
 
 instance Semigroup (ParseResult tok a) where
@@ -46,8 +53,11 @@ instance Semigroup (ParseResult tok a) where
       (ParseEmpty {}, _)  -> r
       (ParseResult {}, _) -> l
 
+-- | The amazing stream parsing monad! This monad is comparable to a
+-- combination of StateT, ExceptT, and MaybeT. It tracks state (the
+-- current context and the stream state) and handles pure exceptions.
 newtype StreamParser tok a = StreamParser
-  { runStreamParser :: [Context] -> [tok] -> ParseResult tok a }
+  { runStreamParser :: [Builder] -> [tok] -> ParseResult tok a }
   deriving (Functor)
 
 instance Applicative (StreamParser tok) where
@@ -81,53 +91,71 @@ instance MonadError Builder (StreamParser tok) where
 --------------------------------------------------------------------------------
 
 -- | Format an error message prepended with context information.
-errorInContext :: [Context] -> Builder -> Builder
+errorInContext :: [Builder] -> Builder -> Builder
 errorInContext contexts =
   mconcat
   . List.reverse
   . List.intersperse ": "
   . (: contexts)
 
-getContexts :: StreamParser tok [Context]
+-- | Access the context stack.
+getContexts :: StreamParser tok [Builder]
 getContexts = StreamParser $ \cs ts -> ParseResult cs ts cs
 
-pushContext :: Context -> StreamParser tok ()
+-- | Push a new context to the context stack.
+pushContext :: Builder -> StreamParser tok ()
 pushContext context = StreamParser $ \cs ts -> ParseResult (context : cs) ts ()
 
-popContext :: StreamParser tok (Maybe Context)
+-- | Pop the top context from the stack, if there is one.
+popContext :: StreamParser tok (Maybe Builder)
 popContext = StreamParser $ \cs ts ->
   case cs of
     (c : cs') -> ParseResult cs' ts $ Just c
     _         -> ParseResult cs ts Nothing
 
-withContext :: Context -> StreamParser tok a -> StreamParser tok a
+-- | Push the given context onto the stack, perform a computation,
+-- then pop it off. This assumes the computation doesn't modify the
+-- stack.
+withContext :: Builder -> StreamParser tok a -> StreamParser tok a
 withContext context action = pushContext context *> action <* popContext
 
 --------------------------------------------------------------------------------
 
+-- | Remove and return the first token in the stream.
 popMaybe :: StreamParser tok (Maybe tok)
 popMaybe = StreamParser $ \cs ts ->
   case ts of
     (t:ts') -> ParseResult cs ts' $ Just t
     _       -> ParseResult cs ts Nothing
 
+-- | View the first token in the stream without consuming it.
 peekMaybe :: StreamParser tok (Maybe tok)
 peekMaybe = StreamParser $ \cs ts ->
   case ts of
     (t:_) -> ParseResult cs ts $ Just t
     _     -> ParseResult cs ts Nothing
 
+-- | Remove and return the first token in the stream. Evaluates to
+-- 'empty' if there are no tokens in the stream.
 pop :: StreamParser tok tok
 pop = StreamParser $ \cs ts ->
   case ts of
     (t:ts') -> ParseResult cs ts' t
     _       -> ParseEmpty cs ts
 
+-- | View the first token in the stream without consuming it.
+-- Evaluates to 'empty' if there are no tokens in the stream.
 peek :: StreamParser tok tok
 peek = StreamParser $ \cs ts ->
   case ts of
     (t:_) -> ParseResult cs ts t
     _     -> ParseEmpty cs ts
 
+-- | Prepend a token to the front of the stream.
 push :: tok -> StreamParser tok ()
 push t = StreamParser $ \cs ts -> ParseResult cs (t:ts) ()
+
+-- | Pop the first token and discard it. Nothing happens if there are
+-- no tokens in the stream.
+pop_ :: StreamParser tok ()
+pop_ = StreamParser $ \cs ts -> ParseResult cs (drop 1 ts) ()
