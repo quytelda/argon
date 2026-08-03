78e7586440c5d2ca1b773719bdc93bc787e53914
Quytelda Kahja <quytelda@tamalin.org>
2026-08-03 00:40:29 -0700

    test: Fix ParseTree tests

--- test/Mangrove/ParseTreeSpec.hs
+++ test/Mangrove/ParseTreeSpec.hs
@@ -18,62 +18,62 @@ spec :: Spec
 spec = do
   describe "pure" $ do
     it "resolves to the given value" $ do
-      runArgumentParser (ValueNode 'a' :: ParseTree UnixScheme Char) []
+      runHelpfulParser_ (ValueNode 'a' :: ParseTree UnixScheme Char) []
         `shouldBe` Success [] 'a'
 
   describe "liftA2" $ do
     it "combines two values" $ do
-      runArgumentParser (liftA2 (+) (pure 1) (pure 2) :: ParseTree UnixScheme Int) []
+      runHelpfulParser_ (liftA2 (+) (pure 1) (pure 2) :: ParseTree UnixScheme Int) []
         `shouldBe` Success [] 3
 
       -- should be equivalent
-      runArgumentParser ((+) <$> pure 1 <*> pure 2 :: ParseTree UnixScheme Int) []
+      runHelpfulParser_ ((+) <$> pure 1 <*> pure 2 :: ParseTree UnixScheme Int) []
         `shouldBe` Success [] 3
 
   describe "empty" $ do
     it "doesn't resolve to any value" $ do
-      runArgumentParser (empty :: ParseTree UnixScheme Char) []
-        `shouldBe` Failure [] "empty"
+      runHelpfulParser_ (empty :: ParseTree UnixScheme Char) []
+        `shouldBe` Failure "empty"
 
   describe "(<|>)" $ do
     context "when the left child is resolvable" $ do
       it "resolves as the left child" $ do
-        runArgumentParser (pure "asdf" <|> opt_e_param) []
+        runHelpfulParser_ (pure "asdf" <|> opt_e_param) []
           `shouldBe` Success [] "asdf"
 
         -- When the right child is also resolvable, it should be
         -- ignored.
-        runArgumentParser (pure "asdf" <|> pure "qwer" :: ParseTree UnixScheme Text) []
+        runHelpfulParser_ (pure "asdf" <|> pure "qwer" :: ParseTree UnixScheme Text) []
           `shouldBe` Success [] "asdf"
 
     context "when the left child is unresolvable" $ do
       it "resolves as the right child" $ do
-        runArgumentParser (opt_e_param <|> pure "asdf") []
+        runHelpfulParser_ (opt_e_param <|> pure "asdf") []
           `shouldBe` Success [] "asdf"
 
     context "when one child is triggered" $ do
       it "prunes the other child" $ do
-        runArgumentParser (opt_e_unit <|> opt_f_unit) ["-e", "-f"]
+        runHelpfulParser_ (opt_e_unit <|> opt_f_unit) ["-e", "-f"]
           `shouldBe` Success ["-f"] ()
-        runArgumentParser (opt_e_unit <|> opt_f_unit) ["-f", "-e"]
+        runHelpfulParser_ (opt_e_unit <|> opt_f_unit) ["-f", "-e"]
           `shouldBe` Success ["-e"] ()
 
   describe "many" $ do
     it "parses multiple instances" $ do
-      runArgumentParser (many opt_e_param) ["-e", "asdf", "-e", "qwer", "-e", "zxcv"]
+      runHelpfulParser_ (many opt_e_param) ["-e", "asdf", "-e", "qwer", "-e", "zxcv"]
         `shouldBe` Success [] ["asdf", "qwer", "zxcv"]
     it "parses zero instances" $ do
-      runArgumentParser (many opt_e_param) ["blah"]
+      runHelpfulParser_ (many opt_e_param) ["blah"]
         `shouldBe` Success ["blah"] []
 
     it "handles compound trees" $ do
       let tree = (opt_f_unit *> opt_e_param) <|> opt_example_param
-      runArgumentParser (many tree) ["-f", "-e", "asdf", "--example", "qwer"]
+      runHelpfulParser_ (many tree) ["-f", "-e", "asdf", "--example", "qwer"]
         `shouldBe` Success [] ["asdf", "qwer"]
 
     it "doesn't swallow arguments" $ do
-      runArgumentParser (many $ opt_f_unit *> opt_e_param) ["-f", "-e", "asdf", "-f"]
-        `shouldBe` Failure [] "expected: -e"
+      runHelpfulParser_ (many $ opt_f_unit *> opt_e_param) ["-f", "-e", "asdf", "-f"]
+        `shouldBe` Failure "expected: -e"
         -- Some attempts at implementing many/some resulted in
         -- arguments being silently swallowed if they were consumed by
         -- a parser inside a ManyNode which didn't receive enough
@@ -84,25 +84,25 @@ spec = do
 
   describe "some" $ do
     it "parses multiple instances" $ do
-      runArgumentParser (some opt_e_param) ["-e", "asdf", "-e", "qwer", "-e", "zxcv"]
+      runHelpfulParser_ (some opt_e_param) ["-e", "asdf", "-e", "qwer", "-e", "zxcv"]
         `shouldBe` Success [] ["asdf", "qwer", "zxcv"]
     it "requires at least one instance" $ do
-      runArgumentParser (some opt_e_param) ["blah"]
-        `shouldBe` Failure [] "unexpected blah"
+      runHelpfulParser_ (some opt_e_param) ["blah"]
+        `shouldBe` Failure "unexpected blah"
 
     it "handles compound trees" $ do
       let tree = (opt_f_unit *> opt_e_param) <|> opt_example_param
-      runArgumentParser (some tree) ["-f", "-e", "asdf", "--example", "qwer"]
+      runHelpfulParser_ (some tree) ["-f", "-e", "asdf", "--example", "qwer"]
         `shouldBe` Success [] ["asdf", "qwer"]
 
     it "doesn't swallow arguments" $ do
-      runArgumentParser (some $ opt_f_unit *> opt_e_param) ["-f", "-e", "asdf", "-f"]
-        `shouldBe` Failure [] "expected: -e"
+      runHelpfulParser_ (some $ opt_f_unit *> opt_e_param) ["-f", "-e", "asdf", "-f"]
+        `shouldBe` Failure "expected: -e"
 
   describe "optional" $ do
     it "parses exactly one instance" $ do
-      runArgumentParser (optional opt_e_param) ["-e", "asdf", "-e", "qwer", "-e", "zxcv"]
+      runHelpfulParser_ (optional opt_e_param) ["-e", "asdf", "-e", "qwer", "-e", "zxcv"]
         `shouldBe` Success [ "-e", "qwer", "-e", "zxcv"] (Just "asdf")
     it "parses zero instances" $ do
-      runArgumentParser (optional opt_e_param) ["blah"]
+      runHelpfulParser_ (optional opt_e_param) ["blah"]
         `shouldBe` Success ["blah"] Nothing
